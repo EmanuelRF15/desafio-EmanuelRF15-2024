0df0fc0b9627a75393cc0e514b5d7054a734839f
EmanuelF15 <emanuelricardo333@gmail.com>
2024-09-08 20:54:03 -0300

    added analizaRecintos

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,14 +1,14 @@
 class RecintosZoo {
     constructor (){
         this.recintos = [
-            { numero: 1, bioma: "savana", capacidade: 10, animais: [{especie: "MACACO", quantidade: 3, tamnaho: 1}] },
+            { numero: 1, bioma: "savana", capacidade: 10, animais: [{especie: "MACACO", quantidade: 3, tamanho: 1}] },
             { numero: 2, bioma: "floresta", capacidade: 5, animais: [] },
             { numero: 3, bioma: "savana e rio", capacidade: 7, animais: [{especie: "GAZELA", quantidade: 1, tamanho: 2}] },
             { numero: 4, bioma: "rio", capacidade: 8, animais: [] },
-            { numero: 5, bioma: "savana", capacidade: 9, animais: [{especie: "LEAO", quantidade: 1, tamnaho: 3}] },
+            { numero: 5, bioma: "savana", capacidade: 9, animais: [{especie: "LEAO", quantidade: 1, tamanho: 3}] },
         ];
 
-        this.animal = [
+        this.animaisPermitidos = [
             {especie: "LEAO", tamanho: 3, biomas: ["savana"], carnivoro: true},
             {especie: "LEOPARDO", tamanho: 2, biomas: ["savana"], carnivoro: true},
             {especie: "CROCODILO", tamanho: 3, biomas: ["rio"], carnivoro: true},
@@ -19,7 +19,35 @@ class RecintosZoo {
     }
 
     analisaRecintos(animal, quantidade) {
+        if(!this.animaisPermitidos[animal]) {
+            return { erro: "Animal inválido", recintosViaveis: null };
+        }
 
+        if(quantidade < 1) {
+            return { erro: "Quantidade inválida", recintosViaveis: null };
+        }
+
+        const infoAnimal = this.animaisPermitidos[animal];
+        const recintosViaveis = [];
+        const espacosNecessarios = quantidade * infoAnimal.tamanho;
+
+        for(const recinto of this.recintos) {
+            let espacoOcupado = recinto.animais.reduce((total, a) => total + (a.tamanho * a.quantidade), 0);
+            let espacoExtra = (recinto.animais.length > 0 && infoAnimal.carnivoro===false) ? 1 : 0;
+            if(!infoAnimal.biomas.includes(recinto.bioma) || recinto.animais.length > 0 && infoAnimal.carnivoro===true || infoAnimal.especie === "HIPOPOTAMO" && recinto.bioma !== "savana e rio") {
+                continue;
+            }
+
+            const espacoDisponivel = recinto.capacidade - espacoOcupado - espacoExtra;
+            if(espacoDisponivel >= espacosNecessarios) {
+                recintosViaveis.push(`Recinto ${recinto.numero} (espaço livre: ${espacoDisponivel - espacosNecessarios} total: ${recinto.capacidade})`);
+            }
+        }
+
+        if(recintosViaveis.length === 0) {
+            return { erro: "Não há recintos viáveis", recintosViaveis: null };
+        }
+        return { erro: null, recintosViaveis: recintosViaveis.sort() };
     }
 
 }
